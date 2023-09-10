1c326e82a3b8fe7bc05f145c2f4371b0d3e161a4
NoahOnFyre <105240543+NoahOnFyre@users.noreply.github.com>
2023-09-10 19:16:18 +0200

    Add main menu and repo input.

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import "github.com/NoahOnFyre/gengine"
+
 func main() {
-    
+	current, _ := od.Getwd()
+    logging.Print("✨ adrenaline ✨ - install golang applications with ease")
+	repo := logging.Input("github repository (owner/repo@branch): ")
 }
